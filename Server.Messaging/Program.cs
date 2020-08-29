0e0db59d56e1eeaaa11820fa9b106d255454c850
MajidImani <MajidImani2012@gmail.com>
2020-08-29 12:33:11 +0430

    remove comments

--- Server.Messaging/Program.cs
+++ Server.Messaging/Program.cs
@@ -9,23 +9,19 @@ namespace Server.Messaging
 {
     public class Program
     {
-        //تنها روم برنامه ایجاد میشود
         private static Room Room = new Room("Public_ChatRoom");
         public static void Main(string[] args)
         {
-            //سرور روی پورت 6009 فال گوش می ایستد
             TcpSocketServer ss = new TcpSocketServer(new ChatRoomService(), 6009);
             TcpListener tcpListener = ss.CreateListener(6009);
             tcpListener.Start();
             while (true)
             {
-                //به محض اینکه کلاینت کانکشن زد در یک نخ یا ترد جدا کارهای مربوط به آن کلاینت را هندل می کند
                 TcpClient client = ss.WaitForConnection(tcpListener);
                 Task.Run(() => ProcessMessages(ss, client));
             }
         }
 
-        //هندل کردن کارهای کلاینت
         private static void ProcessMessages(TcpSocketServer ss, TcpClient client)
         {
             while (true)
