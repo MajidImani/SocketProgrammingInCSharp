ac31723e6767bf691672fce0a80cb8b63553bc72
MajidImani <MajidImani2012@gmail.com>
2020-09-03 21:21:28 +0430

    left the room action tests

--- ChatRoom.Tests/ChatRoomServiceTests.cs
+++ ChatRoom.Tests/ChatRoomServiceTests.cs
@@ -16,11 +16,12 @@ namespace ChatRoom.Tests
             _chatRoomService = new ChatRoomService();
         }
 
+
         [Theory]
         [InlineData("aUser", "Public_ChatRoom")]
         [InlineData("bUser", "Public_ChatRoom")]
         [InlineData("cUser", "Public_ChatRoom")]
-        public void Join_To_Room_Join_New_User_And_Connections_To_Room_When(string username, string roomName)
+        public void Join_To_Room_Join_New_User_And_Connections_To_Room(string username, string roomName)
         {
             Chat.Rooms = new List<Room>();
             TcpClient tcpClient = new TcpClient();
@@ -31,13 +32,45 @@ namespace ChatRoom.Tests
             Chat.TcpSocketClients.Should().Contain(tcpClient);
             Chat.UserRooms.Should().Contain(new KeyValuePair<string, string>(username, room.Name));
             Chat.UserSockets.Should().Contain(new KeyValuePair<string, TcpClient>(username, tcpClient));
+            ClearTestData();
         }
 
         [Theory]
         [InlineData("aUser")]
         public void Is_Duplicate_User_Validate_User_Duplication_If_New_User_Is_Exist(string username)
         {
-            username.Should().Be("aUser");
+            Chat.RoomUsers = new List<string>();
+            _chatRoomService.IsDuplicateUser(username).Should().Be(false);
+            Chat.RoomUsers.Add(username);
+            _chatRoomService.IsDuplicateUser(username).Should().Be(true);
+            ClearTestData();
+        }
+
+        [Theory]
+        [InlineData("aUser", "Public_ChatRoom")]
+        public void Left_The_Room_Should_Left_User_And_Connections_From_Room(string username, string roomName)
+        {
+            Chat.Rooms = new List<Room>();
+            TcpClient tcpClient = new TcpClient();
+            var room = new Room(roomName);
+            _chatRoomService.JoinToRoom(tcpClient, username, room);
+            tcpClient.Client.Should().NotBe(null);
+            _chatRoomService.LeftTheRoom(tcpClient, username, room);
+            Chat.RoomUsers.Should().NotContain(username);
+            Chat.TcpSocketClients.Should().NotContain(tcpClient);
+            Chat.UserRooms.Should().NotContain(new KeyValuePair<string, string>(username, room.Name));
+            Chat.UserSockets.Should().NotContain(new KeyValuePair<string, TcpClient>(username, tcpClient));
+            tcpClient.Client.Should().Be(null);
+            ClearTestData();
+        }
+
+        private static void ClearTestData()
+        {
+            Chat.Rooms.Clear();
+            Chat.RoomUsers.Clear();
+            Chat.UserRooms.Clear();
+            Chat.UserSockets.Clear();
+            Chat.TcpSocketClients.Clear();
         }
     }
 }
